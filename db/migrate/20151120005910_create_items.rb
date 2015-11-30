1d526a23ac4bb39f4f03d06233b36ce27f390ece
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-11-30 15:58:10 -0800

    Updated migration table

--- db/migrate/20151120005910_create_items.rb
+++ db/migrate/20151120005910_create_items.rb
@@ -2,6 +2,8 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :description
+      t.references :list, index: true, foreign_key: true
+      t.boolean :completed, default: false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,22 @@ ActiveRecord::Schema.define(version: 20151120010014) do
 
   create_table "items", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "list_id"
+    t.boolean  "completed",   default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
+
+  add_index "items", ["list_id"], name: "index_items_on_list_id"
 
   create_table "lists", force: :cascade do |t|
+    t.string   "name"
     t.integer  "user_id"
-    t.integer  "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "permissions", default: "private"
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
-  add_index "lists", ["item_id"], name: "index_lists_on_item_id"
   add_index "lists", ["user_id"], name: "index_lists_on_user_id"
 
   create_table "users", force: :cascade do |t|
