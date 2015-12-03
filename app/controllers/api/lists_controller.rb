73d22292957b30efdf18f9c55d1ffef5a614f9fd
rlee01@wesleyan.edu <rlee01@wesleyan.edu>
2015-12-03 11:21:35 -0800

    Added delete/destroy functionality

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -12,6 +12,17 @@ class Api::ListsController < ApiController
       render json: {errors: list.errors.full_messages}, status: :unprocessable_entity
     end
   end
+  
+  def destroy
+    begin
+      list = List.find(params[:id])
+      list.destroy
+
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
+  end
 
   private
   def list_params
